924154017a158663ca63f8d5d1d0f87d0b6ea551
idevyami <130596184+idevyami@users.noreply.github.com>
2025-07-10 00:09:46 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
-  try {
-    res.status(200).json({ success: true, message: "API سالمه 👌🏻" });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+  res.status(200).json({
+    success: true,
+    message: "Vercel API is working 🟢"
+  });
 }
